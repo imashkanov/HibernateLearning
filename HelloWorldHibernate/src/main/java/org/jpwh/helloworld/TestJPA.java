a3b826dbb06ff2344057f1b015acd9de70dc5f9f
Gleb Mashkanov <gleb.mashkanov@gmail.com>
2020-09-16 11:13:19 +0300

    Корректное получение записей из базы и корректный update

---
+++ HelloWorldHibernate/src/main/java/org/jpwh/helloworld/TestJPA.java
@@ -0,0 +1,46 @@
+package org.jpwh.helloworld;
+
+import org.jpwh.model.helloworld.Message;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class TestJPA {
+
+  public static void main(String[] args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("HelloWorldPU");
+    EntityManager em = emf.createEntityManager();
+    createMessages(em);
+    List<Message> messages = getCreatedMesages(em);
+    System.out.printf("Messages: %s\n", messages);
+    updateMessage(em, messages.get(0));
+    System.out.printf("Updated first element. Messages after update: %s\n", messages);
+    em.close();
+  }
+
+  private static void updateMessage(EntityManager em, Message message) {
+    EntityTransaction transaction = em.getTransaction();
+    transaction.begin();
+    message.setText("Changed testMessage");
+    transaction.commit();
+  }
+
+  private static List<Message> getCreatedMesages(EntityManager em) {
+    return (List<Message>) em.createQuery("select m from Message m").getResultList();
+  }
+
+  private static void createMessages(EntityManager em) {
+    Message message1 = new Message();
+    message1.setText("testMessage1");
+    Message message2 = new Message();
+    message2.setText("testMessage2");
+    EntityTransaction transaction = em.getTransaction();
+    transaction.begin();
+    em.persist(message1);
+    em.persist(message2);
+    transaction.commit();
+  }
+}
