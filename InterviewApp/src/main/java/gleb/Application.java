2dac1aca260e9b253c6a9a3eab57afa82f362c1f
Gleb Mashkanov <gleb.mashkanov@gmail.com>
2020-11-04 22:52:17 +0300

    Работает проект spring

--- InterviewApp/src/main/java/gleb/Application.java
+++ InterviewApp/src/main/java/gleb/Application.java
@@ -1,10 +1,15 @@
 package gleb;
 
+import gleb.Config.hibernate.HibernateUtils;
+import org.hibernate.Session;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 
 //@Configuration
 //@EnableAutoConfiguration
@@ -12,25 +17,49 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 public class Application {
 
+  private Session hibernateSession;
+
 //	private static Log logger = LogFactory.getLog(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-//	@Bean
-//	protected ServletContextListener listener() {
-//		return new ServletContextListener() {
-//			@Override
-//			public void contextInitialized(ServletContextEvent sce) {
-//				logger.info("ServletContext initialized");
-//			}
-//
-//			@Override
-//			public void contextDestroyed(ServletContextEvent sce) {
-//				logger.info("ServletContext destroyed");
-//			}
-//		};
-//	}
+	@Bean
+	protected ServletContextListener listener() {
+		return new ServletContextListener() {
+			@Override
+			public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("ServletContext initialized");
+			}
 
+			@Override
+			public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("ServletContext destroyed");
+			}
+		};
+	}
+
+  @Bean
+  public Session getHibernateSession() {
+    return hibernateSession;
+  }
+
+  @PostConstruct
+  public void initHibernateSession() {
+    try {
+      hibernateSession = HibernateUtils.getSessionFactory().openSession();
+    } catch (Exception e) {
+//      logger.log(e);
+    }
+  }
+
+  @PreDestroy
+  public void destroyHibernateSession() {
+    try {
+      HibernateUtils.shutdown();
+    } catch (Exception e) {
+//      logger.log(e);
+    }
+  }
 }
--- InterviewApp/src/main/java/gleb/repositories/SkillRepository.java
+++ InterviewApp/src/main/java/gleb/repositories/SkillRepository.java
@@ -1,18 +1,15 @@
 package gleb.repositories;
 
-import gleb.dto.Skill;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public class SkillRepository implements CrudRepository<Skill, Long> {
+public class SkillRepository  {
 
   Session hibernateSession;
 
@@ -22,63 +19,9 @@ public class SkillRepository implements CrudRepository<Skill, Long> {
   }
 
   public List<String> getSkillsList(HttpServletRequest request) {
-    String query = "from Skill";
-    Query getSkillsListQuery = hibernateSession.createQuery(query);
+    String query = "select * from Skill";
+    Query getSkillsListQuery = hibernateSession.createNativeQuery(query);
     return getSkillsListQuery.list();
   }
 
-  @Override
-  public <S extends Skill> S save(S s) {
-    return null;
-  }
-
-  @Override
-  public <S extends Skill> Iterable<S> saveAll(Iterable<S> iterable) {
-    return null;
-  }
-
-  @Override
-  public Optional<Skill> findById(Long aLong) {
-    return Optional.empty();
-  }
-
-  @Override
-  public boolean existsById(Long aLong) {
-    return false;
-  }
-
-  @Override
-  public Iterable<Skill> findAll() {
-    return null;
-  }
-
-  @Override
-  public Iterable<Skill> findAllById(Iterable<Long> iterable) {
-    return null;
-  }
-
-  @Override
-  public long count() {
-    return 0;
-  }
-
-  @Override
-  public void deleteById(Long aLong) {
-
-  }
-
-  @Override
-  public void delete(Skill skill) {
-
-  }
-
-  @Override
-  public void deleteAll(Iterable<? extends Skill> iterable) {
-
-  }
-
-  @Override
-  public void deleteAll() {
-
-  }
 }
